1340708e295b5340a170a742a18032e5db645c2d
Cristian Zabaleta <zabaletac3@gmail.com>
2026-06-24 09:30:08 -0500

    feat(constants): add url for hrpc micro organization

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export default {
   KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID || 'micro-users',
   KAFKA_GROUP_ID: process.env.KAFKA_GROUP_ID || 'micro-users-group',
   GRPC_URL_USER: process.env.GRPC_URL_USER || 'localhost:50052',
-  GRPC_URL_ORGANIZATION: process.env.GRPC_URL_ORGANIZATION || 'localhost:50052',
+  GRPC_URL_ORGANIZATION: process.env.GRPC_URL_ORGANIZATION || 'localhost:50098',
   OPENAI_API_KEY: process.env.OPENAI_API_KEY!,
   RESEND_API_KEY: process.env.RESEND_API_KEY!,
   RESEND_FROM_EMAIL: process.env.RESEND_FROM_EMAIL!,
